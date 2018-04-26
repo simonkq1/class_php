beb9930e7521893ddaf59eb224a82d4a9e4ddd52
Simon <https://github.com/simonkq1/class_php.git>
2018-04-26 21:55:59 +0800

    2018.04.26-home2

--- 2018.04.26/HW2.php
+++ 2018.04.26/HW2.php
@@ -1,32 +1,13 @@
-
-
-<form>
-<input type="text" name="y" value="">
-
-<input type="submit" value="="/>
-</form>
-
 <?php
-$y = $_GET['y'];
-$z = $y%4;
-$x = $y%100;
-$c = $y%400;
+    $v1 = 50;
+    $v2 = 3;
+    $v3 = $v1/$v2;
+    $v4 = floor($v3);
+    $v5 = $v1%$v2;
+    echo  "{$v1} / {$v2} = {$v4}.....{$v5} <hr>";
 
-if($z==0 && $x==0 && $c==0){
-    echo '閏年';
-}else if($z==0 && $x==0){
-    echo '不是閏年';
-}else if($z==0){
-    echo '閏年';
-}else {
-
-    echo '不是閏年';
-}
 
 ?>
-<!--z=0  y-->
-<!--z=0 x=0 n-->
-<!--z=0 x=0 y=0 y-->
-<!---->
-<!--echo '閏年';-->
-<!--echo '不是閏年';-->
+
+
+
