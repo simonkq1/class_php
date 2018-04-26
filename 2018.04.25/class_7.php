b3bf53cd83d000dc0e7a12c8984b712c8d2899dd
simonkq1 <simonkq1@gmail.com>
2018-04-26 10:12:22 +0800

    2018.04.25-1

--- 2018.04.25/class_7.php
+++ 2018.04.25/class_7.php
@@ -1,6 +1,44 @@
-<form action="class_8.php">
-    <input type="text" name="x"/>
-    +
+<?php
+    $x = '';
+    $y = '';
+    $z = '';
+if (isset($_GET['x'])&&isset($_GET['y'])){
+
+
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        $op = $_GET['op'];
+        if ($op=='1'){
+            $z = $x+$y;
+        }
+        if ($op=='1'){
+            $z = $x+$y;
+        }        if ($op=='1'){
+            $z = $x+$y;
+        }        if ($op=='1'){
+            $z = $x+$y;
+        }
+
+//    echo "<b> {$x} + {$y} = {$z} </b><br>";
+}
+
+?>
+
+
+
+
+<form>
+    <input type="text" name="x" value="<?php echo $x; ?>"  />
+    <select name="op">
+        <option value="1">+</option>
+        <option value="2">-</option>
+        <option value="3">*</option>
+        <option value="4">/</option>
+    </select>
     <input type="text" name="y"/>
     <input type="submit"  value="="/>
+
+    <?php
+    echo "<input type=\"text\" name=\"a\" value='{$z}'/>";
+    ?>
 </form>
--- 2018.04.25/class_8.php
+++ 2018.04.25/class_8.php
@@ -1,11 +1,12 @@
 <?php
+    if (isset($_GET['x'])&&isset($_GET['y'])){
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        $z = $x+$y;
+        echo "<b> {$x} + {$y} = {$z} </b><br>";
 
-    $x = $_GET['x'];
-    $y = $_GET['y'];
-    echo $x . ':' . gettype($x).'<br>';
 
-
-    var_dump($x);
+    }
 
 ?>
 
