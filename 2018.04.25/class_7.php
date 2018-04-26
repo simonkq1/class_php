6518b978e9e4d0a9da99632339b1ecc2775ecd23
simonkq1 <simonkq1@gmail.com>
2018-04-26 10:26:20 +0800

    2018.04.25-1

--- 2018.04.25/class_7.php
+++ 2018.04.25/class_7.php
@@ -1,7 +1,7 @@
 <?php
-    $x = '';
-    $y = '';
-    $z = '';
+    $x = $y = $z = '';
+    $op = 1;
+
 if (isset($_GET['x'])&&isset($_GET['y'])){
 
 
@@ -11,12 +11,14 @@ if (isset($_GET['x'])&&isset($_GET['y'])){
         if ($op=='1'){
             $z = $x+$y;
         }
-        if ($op=='1'){
-            $z = $x+$y;
-        }        if ($op=='1'){
-            $z = $x+$y;
-        }        if ($op=='1'){
-            $z = $x+$y;
+        else if ($op=='2'){
+            $z = $x-$y;
+        }
+        else if ($op=='3'){
+            $z = $x*$y;
+        }
+        else if ($op=='4'){
+            $z = $x/$y;
         }
 
 //    echo "<b> {$x} + {$y} = {$z} </b><br>";
@@ -30,12 +32,12 @@ if (isset($_GET['x'])&&isset($_GET['y'])){
 <form>
     <input type="text" name="x" value="<?php echo $x; ?>"  />
     <select name="op">
-        <option value="1">+</option>
-        <option value="2">-</option>
-        <option value="3">*</option>
-        <option value="4">/</option>
+        <option value="1" <?php if($op==1) {echo 'selected';}?>>+</option>
+        <option value="2" <?php if($op==2) {echo 'selected';}?>>-</option>
+        <option value="3" <?php if($op==3) {echo 'selected';}?>>*</option>
+        <option value="4" <?php if($op==4) {echo 'selected';}?>>/</option>
     </select>
-    <input type="text" name="y"/>
+    <input type="text" name="y" value="<?php echo $y; ?>" />
     <input type="submit"  value="="/>
 
     <?php
