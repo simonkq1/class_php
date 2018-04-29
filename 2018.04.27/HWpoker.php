057007fc63d230807ea4f15f7b0c8476a5197b3e
Simon <https://github.com/simonkq1/class_php.git>
2018-04-29 18:27:55 +0800

    2018.04.28-home3

--- 2018.04.27/HWpoker.php
+++ 2018.04.27/HWpoker.php
@@ -1,1 +1,35 @@
 <!--//poker1 洗牌-->
+<?php
+
+//$poker =array();
+//for($i=1;$i<=52;$i++)
+//    $poker[] =$i;
+//var_dump($poker);
+
+//
+
+for ($i = 0; $i < 52; $i++) {
+    $r = rand(0, 51);
+
+
+//    檢查
+    $isRepeat = false;
+    for ($j = 0; $j < $i; $j++) {
+        if ($poker[$j] == $r) {
+            //重複了
+            $isRepeat = true;
+            break;
+        }
+    }
+    if (!$isRepeat) {
+
+        $poker[$i] = $r;
+    } else {
+        $i--;
+    }
+}
+foreach ($poker as $v) {
+    echo "{$v}<br>";
+}
+
+
