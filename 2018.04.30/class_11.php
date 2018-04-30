e045475d494dfa41b993b1e5c5e09f5fa2086a8b
simonkq1 <simonkq1@gmail.com>
2018-04-30 14:58:25 +0800

    2019.04.30-class5

--- 2018.04.30/class_11.php
+++ 2018.04.30/class_11.php
@@ -1,6 +1,11 @@
 <?php
-require_once ('class_9.php');
+require_once('class_9.php');
 require_once 'class_10.php';
 
 echo 'Hello, World<br>';
-echo myurl1;
+echo myurl1.'<br>';
+
+$r = 4;
+$area = PI * ($r ** 2);
+
+echo $area;
--- 2018.04.30/class_9.php
+++ 2018.04.30/class_9.php
@@ -3,3 +3,6 @@
 //常數 [a-zA-Z][a-zA-Z0-9_]*   //可中文
 define('myurl1', 'http://www.iii.org.tw');
 define('myurl2', 'http://www.brad.tw');
+
+
+const PI = 3.14;
