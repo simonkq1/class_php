4262d03723387b252981336325626187e5623a91
simonkq1 <simonkq1@gmail.com>
2018-05-01 11:14:03 +0800

    2019.05.01-class1

--- 2018.05.01/class_3.php
+++ 2018.05.01/class_3.php
@@ -1,3 +1,18 @@
 <?php
-$fp = opendir("test1");
-echo gettype($fp);
+$fp = opendir("/Applications/MAMP/htdocs/2018.05.01/test2/");
+
+while ($file = readdir($fp)){
+    echo $file.'<br>';
+}
+
+closedir($fp);
+
+
+//$s1 = readdir($fp);
+//echo $s1.'<br>';
+//$s1 = readdir($fp);
+//echo $s1.'<br>';
+//$s1 = readdir($fp);
+//echo $s1.'<br>';
+//$s1 = readdir($fp);
+//echo $s1.'<br>';
--- 2018.05.01/test.php
+++ 2018.05.01/test.php
@@ -1,6 +1,8 @@
 <?php
  function numbers(){
+     echo"<tr>";
      for($i=0;$i<100;$i++){
-
+         echo"<td>{$i}</td>";
      }
+     echo"</tr>";
  }
