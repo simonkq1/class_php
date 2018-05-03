1cc4a10413a2feebddc7a8fc7b5eece6a5c389d1
Simon <https://github.com/simonkq1/class_php.git>
2018-05-03 20:50:29 +0800

    2018.05.03-home

--- 2018.05.03/poker.php
+++ 2018.05.03/poker.php
@@ -1,17 +1,17 @@
 <?php
 
-$poker =array();
-$x=array();
-for(;count($x)<51;){
-    $r= rand(1,51);
-    $poker[] =$r;
-    $x=array_unique($poker);
+$poker = array();
+$x = array();
+for (; count($x) < 51;) {
+    $r = rand(1, 51);
+    $poker[] = $r;
+    $x = array_unique($poker);
 }
 //var_dump($poker);
-$c=array_values($x);
+$c = array_values($x);
 
-foreach ($c as $k=>$v){
+foreach ($c as $k => $v) {
     echo "{$k} : {$v}<br>";
 
 }
-echo '<hr>'.count($x);
+echo '<hr>' . count($x);
--- 2018.05.03/poker2.php
+++ 2018.05.03/poker2.php
@@ -1,19 +1,19 @@
 <?php
 
-$a1=range(1,52);
-$a2=array();
-$r=count($a1);
-for ($i=0;$i<$r;$i++){
-    $rand=array_rand($a1);          //$rand是key不是value!!!!!!!!
-    $tmp=$a1[$rand];                //抓出抽取項目放置到$tmp
-    $a1[$rand]=$a1[count($a1)-1];   //將'抽取內容'置換成'最後一個項目'
-    $a1[count($a1)-1]=$tmp;         //將'最後一個項目'置換成'抽取內容'  //完成對調
-    $a2[$i]=$tmp;                   //將抽取項目放置到$a2的第$i項
-    $pop=array_pop($a1);   //刪除$a1最後一個項目 //刪除抽取項目
+$a1 = range(1, 52);
+$a2 = array();
+$r = count($a1);
+for ($i = 0; $i < $r; $i++) {
+    $rand = array_rand($a1);          //$rand是key不是value!!!!!!!!
+    $tmp = $a1[$rand];                //抓出抽取項目放置到$tmp
+    $a1[$rand] = $a1[count($a1) - 1];   //將'抽取內容'置換成'最後一個項目'
+    $a1[count($a1) - 1] = $tmp;         //將'最後一個項目'置換成'抽取內容'  //完成對調
+    $a2[$i] = $tmp;                   //將抽取項目放置到$a2的第$i項
+    $pop = array_pop($a1);   //刪除$a1最後一個項目 //刪除抽取項目
 
 }
 
-foreach ($a2 as $k => $v){
+foreach ($a2 as $k => $v) {
     echo "{$k} : {$v} <br>";
 }
 
