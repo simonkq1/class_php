815940dfe76cbdd07c2ff615abea97fde4803721
Simon <https://github.com/simonkq1/class_php.git>
2018-05-09 22:39:45 +0800

    2018.05.08-home

--- TEST/chatbox.php
+++ TEST/chatbox.php
@@ -1,7 +1,10 @@
-<?php
 
-?>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script>
+    $(document).ready(function(){
 
+    })
+</script>
 <style>
     .chatBox{
         position:relative;
@@ -32,6 +35,20 @@
 <div class="chatArea">
 <form>
     <input type="text" name="chat" id="chat" >
+    <input type="submit" value=">>">
     <button type="button"> >> </button>
 </form>
 </div>
+
+<?php
+
+include_once 'sql.php';
+include_once 'member.php';
+include_once 'Chat.php';
+$chat=$_GET['chat'];
+$sql="insert into chattext (text,date) values ('$chat',now())";
+$result=$mysqli->query($sql);
+$data=$result->fetch_object('chat');
+echo $data->chat;
+
+?>
